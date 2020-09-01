22367d868cb77b22bc75265601b48dd202e87a54
tracer004 <termindustries@gmail.com>
2020-09-01 10:53:11 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,12 @@
+// Comment
+console.log('Hello World');
+let name = 'Ashish';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (~)
+// 'let firstName' is camel notation where 'firstName' is case-sensitive
+
+let firstName;
